41afff099f281b564b38e41d3c71d36ed2262779
Danilo Silva <contact@danilocgsilva.me>
2026-06-29 19:49:54 -0300

    Command shell and command improvement

--- app/src/Commands/RegisterDatabase.php
+++ app/src/Commands/RegisterDatabase.php
@@ -6,13 +6,23 @@ namespace Danilocgsilva\DbCloneCli\Commands;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name:"config:register-database")]
-class RegisterDatabase
+#[AsCommand(
+    name: "config:register-database",
+    description: "Register a database address",
+    help: "Register a database address"
+)]
+class RegisterDatabase extends Command
 {
-    public function __invoke(): int
+    public function __invoke(InputInterface $input, OutputInterface $output): int
     {
-        print("This is my first command");
+        $io = new SymfonyStyle($input, $output);
+        $io->title("Success");
+        $io->text("It is working nicely!");
+        
         return Command::SUCCESS;
     }
 }
