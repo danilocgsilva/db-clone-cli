427cc7eb03e780853bab2edb42e3dac648d014ce
Danilo Silva <contact@danilocgsilva.me>
2026-07-02 06:13:04 -0300

    Using package to andd database entry

--- app/src/Commands/RegisterDatabase.php
+++ app/src/Commands/RegisterDatabase.php
@@ -9,6 +9,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Danilocgsilva\DbCloneCore\Entities\Database;
+use Danilocgsilva\DbCloneCore\BootstrapFactory;
 
 #[AsCommand(
     name: "config:register-database",
@@ -20,8 +22,35 @@ class RegisterDatabase extends Command
     public function __invoke(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $io->title("Success");
-        $io->text("It is working nicely!");
+        $io->title("Database Registration");
+
+        BootstrapFactory::getEntityManager()
+            ->persist(
+                new Database()
+                    ->setHost(
+                        $io->ask("Database host")
+                    )
+                    ->setPassword(
+                        $io->ask("Database password")
+                    )
+                    ->setName(
+                        $io->askHidden("Database name")
+                    )
+            )
+            ->flush();
+
+
+        // $databaseHost = $io->ask("Database host");
+        // $databaseUser = $io->ask("Database user");
+        // $databasePassword = $io->askHidden("Database user");
+
+
+
+        // $io->text("The database host is {$databaseHost}");
+        // $io->text("The database user is {$databaseUser}");
+        // $io->text("The database password is {$databasePassword}");
+
+        $io->success("Database registered successfully!");
         
         return Command::SUCCESS;
     }
